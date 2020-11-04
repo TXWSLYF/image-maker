01acda519157b6838ee36f89604d6ac2a073189d
alex <1060319328@qq.com>
2020-11-04 11:21:07 +0800

    feat: browserRouter -> HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+import { Route, Switch, HashRouter as Router } from 'react-router-dom';
 import EditorPage from './pages/editor/index';
 import PlayPage from './pages/play';
 

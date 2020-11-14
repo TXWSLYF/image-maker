6b7e231667d7b6e53a533fb4ad7c2ca60976e343
alex <1060319328@qq.com>
2020-11-14 14:02:23 +0800

    chore: setupProxy.js 区分不同环境

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,32 +1,47 @@
-// const fs = require('fs');
-// const path = require('path');
+const fs = require('fs');
+const path = require('path');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
+let proxyArg = process.argv.find((arg) => /proxy/.test(arg));
+proxyArg = proxyArg ? proxyArg.split('=')[1] : 'PROD';
+
 module.exports = function (app) {
-  // app.use('/api', async (req, res, next) => {
-  //   const { method, url } = req;
-  //   const [pathName] = url.split('?');
+  let middleware = null;
 
-  //   const absolutePath = path.resolve(__dirname, `./mock${pathName}.json`);
-  //   console.log(method, pathName, method.toLowerCase(), absolutePath);
+  if (proxyArg === 'MOCK') {
+    middleware = async (req, res, next) => {
+      const { method, url } = req;
+      const [pathName] = url.split('?');
 
-  //   if (fs.existsSync(absolutePath)) {
-  //     // 删除模块缓存
-  //     delete require.cache[absolutePath];
-  //     res.end(JSON.stringify(require(absolutePath)));
-  //   }
+      const absolutePath = path.resolve(__dirname, `./mock${pathName}.json`);
+      console.log(method, pathName, method.toLowerCase(), absolutePath);
 
-  //   await next();
-  // });
-  app.use(
-    '/api',
-    createProxyMiddleware({
-      // target: 'http://localhost:8080',
-      // pathRewrite: {
-      //   '^/api': '/', // remove base path
-      // },
+      if (fs.existsSync(absolutePath)) {
+        // 删除模块缓存
+        delete require.cache[absolutePath];
+        res.end(JSON.stringify(require(absolutePath)));
+      }
+
+      await next();
+    };
+  }
+
+  if (proxyArg === 'LOCAL') {
+    middleware = createProxyMiddleware({
+      target: 'http://localhost:8080',
+      pathRewrite: {
+        '^/api': '/', // remove base path
+      },
+      changeOrigin: true,
+    });
+  }
+
+  if (proxyArg === 'PROD') {
+    middleware = createProxyMiddleware({
       target: 'http://image-maker.qsxqd.com',
       changeOrigin: true,
-    }),
-  );
+    });
+  }
+
+  app.use('/api', middleware);
 };

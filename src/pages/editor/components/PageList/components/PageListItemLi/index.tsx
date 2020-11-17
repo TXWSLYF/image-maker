87f27c2a0d8bf5e76a07f82f81b61c1e57e99fb3
alex <1060319328@qq.com>
2020-11-17 11:37:43 +0800

    feat: pageList 组件

--- src/pages/editor/components/PageList/components/PageListItemLi/index.tsx
+++ src/pages/editor/components/PageList/components/PageListItemLi/index.tsx
@@ -1,10 +1,26 @@
 import React, { useMemo } from 'react';
+import { ReactComponent as PageSvg } from 'src/assets/svg/page.svg';
 import styles from './index.module.scss';
 
-const PageListItemLi = () => {
+interface PageListItemLiProps {
+  isActive: boolean;
+  text: string;
+  onClick: () => void;
+}
+
+const PageListItemLi = ({ isActive, onClick, text }: PageListItemLiProps) => {
   return useMemo(() => {
-    return <div className={styles.pageListItemLi}></div>;
-  }, []);
+    return (
+      <li className={`${styles.pageListItemLi} ${isActive ? styles.active : ''}`} onClick={onClick}>
+        <div className={styles.pageName}>
+          <div className={styles.pageListItemLiIcon}>
+            <PageSvg />
+          </div>
+          <span className={styles.editableDiv}>{text}</span>
+        </div>
+      </li>
+    );
+  }, [isActive, onClick, text]);
 };
 
 export default PageListItemLi;
--- src/pages/editor/components/PageList/index.tsx
+++ src/pages/editor/components/PageList/index.tsx
@@ -1,8 +1,9 @@
-import React, { useMemo } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { selectImages } from 'src/features/project/projectSlice';
 import { selectCurImageId, setCurImage, setCurLayers } from 'src/features/editor/editorSlice';
 import PageListHeader from './components/PageListHeader';
+import PageListItemLi from './components/PageListItemLi';
 import styles from './index.module.scss';
 
 function PageList() {
@@ -10,34 +11,38 @@ function PageList() {
   const images = useSelector(selectImages);
   const curImageId = useSelector(selectCurImageId);
 
+  const handleClickPageListItem = useCallback(
+    (id) => {
+      dispatch(setCurImage(id));
+      dispatch(setCurLayers([]));
+    },
+    [dispatch],
+  );
+
   return useMemo(() => {
     return (
       <div className={styles.pageList}>
         <PageListHeader />
-        {images.allIds.map((id) => {
-          const image = images.byId[id];
-          const { name } = image;
+        <ul>
+          {images.allIds.map((id) => {
+            const image = images.byId[id];
+            const { name } = image;
 
-          return (
-            <div
-              onClick={() => {
-                dispatch(setCurImage(id));
-                dispatch(setCurLayers([]));
-              }}
-              className={styles.pageItem}
-              style={{
-                color: id === curImageId ? 'blue' : 'black',
-                background: id === curImageId ? '#cecece' : '#fff',
-              }}
-              key={id}
-            >
-              {name}
-            </div>
-          );
-        })}
+            return (
+              <PageListItemLi
+                key={id}
+                onClick={() => {
+                  handleClickPageListItem(id);
+                }}
+                text={name}
+                isActive={id === curImageId}
+              />
+            );
+          })}
+        </ul>
       </div>
     );
-  }, [images, curImageId, dispatch]);
+  }, [images, curImageId, handleClickPageListItem]);
 }
 
 export default PageList;

6e69173a6ade0bdd8b1e54cb80ec468112599178
alex <1060319328@qq.com>
2020-12-03 15:35:13 +0800

    refactor(textpropertypanel): 优化 TextPropertyPanel 组件

--- src/pages/editor/components/PropertyPanel/TextPropertyPanel/index.tsx
+++ src/pages/editor/components/PropertyPanel/TextPropertyPanel/index.tsx
@@ -1,43 +1,50 @@
+import React, { useMemo } from 'react';
+import { useDispatch } from 'react-redux';
 import { Collapse } from 'antd';
-import React from 'react';
+import ChromeColorPicker from 'src/components/ChromeColorPicker';
+import { setLayersColor } from 'src/features/project/projectUndoableSlice';
 import PropertyRow from '../PropertyRow';
 import './index.scss';
-import ChromeColorPicker from 'src/components/ChromeColorPicker';
-import { useDispatch } from 'react-redux';
-import { setLayersColor } from 'src/features/project/projectUndoableSlice';
 
 const { Panel } = Collapse;
 
-function TextPropertyPanel({ properties, layerId }: { properties: ITextProperties; layerId: string }) {
+export interface TextPropertyPanelProps {
+  properties: ITextProperties;
+  layerId: string;
+}
+
+const TextPropertyPanel = ({ properties, layerId }: TextPropertyPanelProps) => {
   const dispatch = useDispatch();
 
-  return (
-    <div className="textPropertyPanel">
-      <Collapse defaultActiveKey={['1']} expandIconPosition={'right'} style={{ border: 'none', background: '#fff' }}>
-        <Panel header="文本" key="1">
-          <PropertyRow
-            style={{ marginBottom: 10 }}
-            leftChild={
-              <ChromeColorPicker
-                color={properties.color}
-                onChange={(colorResult) => {
-                  dispatch(
-                    setLayersColor({
-                      layerIds: [layerId],
-                      newColor: colorResult.hex,
-                    }),
-                  );
-                }}
-              />
-            }
-            leftLabelText="颜色"
-            rightChild={<div></div>}
-            rightLabelText="纵坐标"
-          />
-        </Panel>
-      </Collapse>
-    </div>
-  );
-}
+  return useMemo(() => {
+    return (
+      <div className="textPropertyPanel">
+        <Collapse defaultActiveKey={['1']} expandIconPosition={'right'} style={{ border: 'none', background: '#fff' }}>
+          <Panel header="文本" key="1">
+            <PropertyRow
+              style={{ marginBottom: 10 }}
+              leftChild={
+                <ChromeColorPicker
+                  color={properties.color}
+                  onChange={(colorResult) => {
+                    dispatch(
+                      setLayersColor({
+                        layerIds: [layerId],
+                        newColor: colorResult.hex,
+                      }),
+                    );
+                  }}
+                />
+              }
+              leftLabelText="颜色"
+              rightChild={<div></div>}
+              rightLabelText="纵坐标"
+            />
+          </Panel>
+        </Collapse>
+      </div>
+    );
+  }, [dispatch, layerId, properties.color]);
+};
 
 export default TextPropertyPanel;

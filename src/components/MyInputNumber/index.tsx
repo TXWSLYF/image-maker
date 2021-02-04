dcdf4c1d15a3db67688a57100d680dd65f51b300
alex <1060319328@qq.com>
2021-02-04 13:32:37 +0800

    feat: myNumberInput 组价功能完善

--- src/components/MyInputNumber/index.tsx
+++ src/components/MyInputNumber/index.tsx
@@ -1,6 +1,8 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { useMemo } from 'react';
 import { useToggle } from 'react-use';
+import { ReactComponent as ArrowUpSvg } from 'src/assets/svg/arrowUp.svg';
+import { ReactComponent as ArrowDownSvg } from 'src/assets/svg/arrowDown.svg';
 import styles from './index.module.scss';
 
 type MyInputNumberProps = {
@@ -10,6 +12,8 @@ type MyInputNumberProps = {
   onPressEnter?: (value: number) => void;
   onBlur?: (value: number) => void;
 };
+
+type Direction = 'up' | 'down';
 
 const reg = /^-?\d*(\.\d*)?$/;
 
@@ -36,16 +40,27 @@ const MyInputNumber = ({ onPressEnter, onBlur, value, text, width = 50 }: MyInpu
 
   const handleKeyDown = useCallback(
     (event: React.KeyboardEvent<HTMLInputElement>) => {
-      // TODO:监听键盘上下箭头事件
+      const out = Number(innerValue);
       if (event.key === 'ArrowUp') {
+        if (typeof out === 'number' && !Number.isNaN(out)) {
+          onPressEnter && onPressEnter(out + 1);
+          setInnerValue(out + 1 + '');
+        } else {
+          setInnerValue(value + '');
+        }
         event.preventDefault();
       }
       if (event.key === 'ArrowDown') {
+        if (typeof out === 'number' && !Number.isNaN(out)) {
+          onPressEnter && onPressEnter(out - 1);
+          setInnerValue(out - 1 + '');
+        } else {
+          setInnerValue(value + '');
+        }
         event.preventDefault();
       }
 
       if (event.key === 'Enter') {
-        const out = Number(innerValue);
         if (typeof out === 'number' && !Number.isNaN(out)) {
           onPressEnter && onPressEnter(Number(innerValue));
           setInnerValue(out + '');
@@ -73,6 +88,47 @@ const MyInputNumber = ({ onPressEnter, onBlur, value, text, width = 50 }: MyInpu
     setIsFocus(true);
   }, [setIsFocus]);
 
+  const handleMyInputNumberHandleMouseDown = useCallback(
+    (direction: Direction) => {
+      const out = Number(innerValue);
+
+      switch (direction) {
+        case 'up': {
+          if (typeof out === 'number' && !Number.isNaN(out)) {
+            onPressEnter && onPressEnter(out + 1);
+            setInnerValue(out + 1 + '');
+          } else {
+            setInnerValue(value + '');
+          }
+
+          break;
+        }
+        case 'down': {
+          if (typeof out === 'number' && !Number.isNaN(out)) {
+            onPressEnter && onPressEnter(out - 1);
+            setInnerValue(out - 1 + '');
+          } else {
+            setInnerValue(value + '');
+          }
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+    },
+    [innerValue, onPressEnter, value],
+  );
+  const handleMyInputNumberHandleMouseUp = useCallback(() => {}, []);
+
+  const handleMyInputNumberHandleUpMouseDown = useCallback(() => {
+    handleMyInputNumberHandleMouseDown('up');
+  }, [handleMyInputNumberHandleMouseDown]);
+
+  const handleMyInputNumberHandleDownMouseDown = useCallback(() => {
+    handleMyInputNumberHandleMouseDown('down');
+  }, [handleMyInputNumberHandleMouseDown]);
+
   return useMemo(() => {
     return (
       <div className={`${styles.myInputNumber} ${isFocus ? styles.myInputNumberFocus : ''}`}>
@@ -86,11 +142,39 @@ const MyInputNumber = ({ onPressEnter, onBlur, value, text, width = 50 }: MyInpu
             width,
           }}
         />
+        <div className={styles.myInputNumberHandleWrap}>
+          <span
+            className={`${styles.myInputNumberHandle} ${styles.myInputNumberHandleUp}`}
+            onMouseDown={handleMyInputNumberHandleUpMouseDown}
+            onMouseUp={handleMyInputNumberHandleMouseUp}
+          >
+            <ArrowUpSvg />
+          </span>
+          <span
+            className={`${styles.myInputNumberHandle} ${styles.myInputNumberHandleDown}`}
+            onMouseDown={handleMyInputNumberHandleDownMouseDown}
+            onMouseUp={handleMyInputNumberHandleMouseUp}
+          >
+            <ArrowDownSvg />
+          </span>
+        </div>
 
         {text ? <span className={styles.myInputNumberText}>{text}</span> : null}
       </div>
     );
-  }, [handleBlur, handleChange, handleFocus, handleKeyDown, innerValue, isFocus, text, width]);
+  }, [
+    handleBlur,
+    handleChange,
+    handleFocus,
+    handleKeyDown,
+    handleMyInputNumberHandleDownMouseDown,
+    handleMyInputNumberHandleMouseUp,
+    handleMyInputNumberHandleUpMouseDown,
+    innerValue,
+    isFocus,
+    text,
+    width,
+  ]);
 };
 
 export default MyInputNumber;
